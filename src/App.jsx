2f03c3109298f87cc602daee541878297a49cd7e
nxn404-n <nafeurrahmanadil@gmail.com>
2024-09-18 12:45:16 +0600

    Add top anime

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TopAni from "./components/TopAni";
 
 function App() {
   return (
     <div className="bg-[#996DAD] h-screen w-screen p-5">
       <Navbar />
+      <TopAni />
     </div>
   );
 }
---
+++ src/components/TopAni.jsx
@@ -0,0 +1,10 @@
+const TopAni = () => {
+  return (
+    <div>
+      <h2 className='text-2xl mt-9 text-white'>Top Anime</h2>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default TopAni;
